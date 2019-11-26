d3969d3f2941cdc2d3b7ba638d8ec591ce7569e5
Kayden Clark <KaydenClark725@gmail.com>
2019-11-26 00:23:28 -0700

    Push to GitHub after adding background boxes

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+
+  render() {
+
+  return (
+    <div className="App">
+      
+      <body className= "bodyLeft">
+        <h1 className= "nameFirst">
+          Kayden
+        </h1>
+      </body>
+      <body className= "bodyRight">
+        <h1 className= "nameLast">
+          Clark
+        </h1>
+      </body>
+    </div>
+  )};
+}
+
